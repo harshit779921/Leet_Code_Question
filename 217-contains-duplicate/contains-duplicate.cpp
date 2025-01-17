bfb84f2bd97a90d24cd2c0837255cd3ec9fc4869
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-01-17 07:52:52 +0530

    Time: 17 ms (94.14%) | Memory: 61.2 MB (77.87%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, int> hash;
-        int n = nums.size();
-        for (int i = 0; i<n; i++) {
-            if (hash[nums[i]] > 0) {
-                return true;  // Duplicate found
+        // Using Hashing
+        // unordered_map<int, int> hash;
+        // int n = nums.size();
+        // for (int i = 0; i<n; i++) {
+        //     if (hash[nums[i]] > 0) {
+        //         return true;  // Duplicate found
+        //     }
+        //     hash[nums[i]]++;  // Mark the number as seen
+        // }
+
+        // return false;  // No duplicates
+
+        // Sorting
+        sort(nums.begin(), nums.end());
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
             }
-            hash[nums[i]]++;  // Mark the number as seen
         }
 
-        return false;  // No duplicates
+        return false;
     }
 };
