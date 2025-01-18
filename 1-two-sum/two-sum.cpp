3f8cf0d62af1a5d30097277b39a2d76c16c9cf51
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-01-18 07:54:20 +0530

    Time: 4 ms (62.43%) | Memory: 15 MB (19.91%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        for(int i = 0;i<n;i++){
-            for(int j = i+1; j<n;j++){
-                if(nums[i]+nums[j] == target){
-                    return {i,j};
-                }
+        // Brute force Approach
+        // int n = nums.size();
+        // for(int i = 0;i<n;i++){
+        //     for(int j = i+1; j<n;j++){
+        //         if(nums[i]+nums[j] == target){
+        //             return {i,j};
+        //         }
+        //     }
+        // }
+        // return {-1,-1};
+
+        // Hashing Approach
+
+        map<int,int>mpp;
+        for(int i =0; i<nums.size(); i++){
+            int a = nums[i];
+            int more = target - a;
+            if(mpp.find(more) != mpp.end()){
+                return {mpp[more],i};
             }
+            mpp[a] = i;
         }
         return {-1,-1};
     }
