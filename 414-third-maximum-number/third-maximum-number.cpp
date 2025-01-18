9d7f6f33e555cc3e4eb098f546701084aabfeb6b
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-01-18 07:01:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (92.35%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,10 +1,33 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        nums.erase(unique(nums.begin(),nums.end()), nums.end());
-        reverse(nums.begin(),nums.end());
-        if(nums.size()<3) return nums[0];
-        return nums[2];
+        // using sorting method
+        // sort(nums.begin(),nums.end());
+        // nums.erase(unique(nums.begin(),nums.end()), nums.end());
+        // reverse(nums.begin(),nums.end());
+        // if(nums.size()<3) return nums[0];
+        // return nums[2];
+
+        // Approach 2: Using Three Variables
+        long first = LONG_MIN, second = LONG_MIN, third = LONG_MIN;
+
+        for (int num : nums) {
+            if (num == first || num == second || num == third) {
+                continue; // Skip duplicates
+            }
+
+            if (num > first) {
+                third = second;
+                second = first;
+                first = num;
+            } else if (num > second) {
+                third = second;
+                second = num;
+            } else if (num > third) {
+                third = num;
+            }
+        }
+
+        return (third == LONG_MIN) ? first : third;
     }
 };
