b919805998209e3ff78853a1ad07e58752c8bfeb
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-01-19 06:26:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (60.61%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        // It is method using built-in-Function
+        // sort(nums.begin(),nums.end());
+        
+        // Dutch National flag Algorithm
+        int low =0, mid = 0, high = nums.size()-1;
+        while(mid<=high){
+            if(nums[mid] == 0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
     }
 };
