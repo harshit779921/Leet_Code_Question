42b45ce15f59c21b0a7eceb634a79d2556d63bc3
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-01-30 05:54:18 +0530

    Time: 0 ms (100.00%) | Memory: 25.3 MB (99.72%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,19 +1,66 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        unordered_map<int, int> freq;
-        for (int i = 0; i < grid.size(); ++i) {
-            for (int j = 0; j < grid.size(); ++j) {
-                freq[grid[i][j]]++;
+        // Better Approach
+        // unordered_map<int, int> freq;
+        // for (int i = 0; i < grid.size(); ++i) {
+        //     for (int j = 0; j < grid.size(); ++j) {
+        //         freq[grid[i][j]]++;
+        //     }
+        // }
+        // int repeat = 0, miss = 0, n = grid.size() * grid.size();
+        // for (int i = 1; i <= n; ++i) {
+        //     if (freq[i] == 2)
+        //         repeat = i;
+        //     if (freq[i] == 0)
+        //         miss = i;
+        // }
+        // return {repeat, miss};
+
+        // Optimal Approach
+        int n = grid.size();
+        int x = 0;
+        int k = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                x ^= grid[i][j];
+                x ^= k;
+                k++;
             }
         }
-        int repeat = 0, miss = 0, n = grid.size() * grid.size();
-        for (int i = 1; i <= n; ++i) {
-            if (freq[i] == 2)
-                repeat = i;
-            if (freq[i] == 0)
-                miss = i;
+        k = 1;
+        while (!(x & k)) {
+            k = k << 1;
         }
-        return {repeat, miss};
+        int a = 0, b = 0;
+        x = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] & k) {
+                    a ^= grid[i][j];
+                } else {
+                    b ^= grid[i][j];
+                }
+
+                if (x & k) {
+                    a ^= x;
+                } else {
+                    b ^= x;
+                }
+                x++;
+            }
+        }
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == a) {
+                    cnt++;
+                }
+            }
+        }
+        if (cnt) {
+            return {a, b};
+        }
+        return {b, a};
     }
 };
