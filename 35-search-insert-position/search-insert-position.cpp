66c110a05419066ba701ac1688063c702d10e183
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-02-18 06:51:29 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (75.77%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -2,14 +2,24 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         // Using STL library
-        return lower_bound(nums.begin(),nums.end(), target) - nums.begin();
+        // return lower_bound(nums.begin(),nums.end(), target) - nums.begin();
 
-        // int n = nums.size();
-        // int low = 0, high =n-1;
+        // Using Non Stl Method
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        int ans = n;
 
-        // while(low <= high){
-        //     int mid = (low+high)/2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] >= target) {
+                ans = mid;
 
-        // }
+                high = mid - 1;
+            } 
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
     }
 };
