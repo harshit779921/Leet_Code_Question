61e77672bba92292385b31a68b6dea9611156f4a
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-02-20 06:36:55 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.07%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,10 +1,42 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        // Linear Search method
+        // int n = nums.size();
+        // for (int i = 0; i<n; i++){
+        //     if(nums[i]==target){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        // Binary Search Method
         int n = nums.size();
-        for (int i = 0; i<n; i++){
-            if(nums[i]==target){
-                return i;
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            // if mid points the target
+            if (nums[mid] == target)
+                return mid;
+
+            // if left part is sorted:
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    // element exists:
+                    high = mid - 1;
+                } else {
+                    // element does not exist:
+                    low = mid + 1;
+                }
+            } else { // if right part is sorted:
+                if (nums[mid] <= target && target <= nums[high]) {
+                    // element exists:
+                    low = mid + 1;
+                } else {
+                    // element does not exist:
+                    high = mid - 1;
+                }
             }
         }
         return -1;
