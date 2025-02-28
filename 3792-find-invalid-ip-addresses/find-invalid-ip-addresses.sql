50c5c0f3fa475286e23eeee652560e7413e521b3
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-02-28 22:23:01 +0530

    Time: 858 ms (7.09%) | Memory: 0B (100.00%) - LeetSync

--- 3792-find-invalid-ip-addresses/find-invalid-ip-addresses.sql
+++ 3792-find-invalid-ip-addresses/find-invalid-ip-addresses.sql
@@ -1,8 +1,7 @@
-# Write your MySQL query statement below
-SELECT ip,
-       count(*) AS invalid_count
-  FROM logs
- WHERE NOT regexp_like(ip, '^(?:[1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(?:[.](?:[1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$')
- GROUP BY ip
- ORDER BY invalid_count DESC,
-          ip DESC;
+SELECT ip, COUNT(*) AS invalid_count
+FROM logs
+WHERE ip REGEXP '(^|\\.)0[0-9]+(\\.|$)'  -- Leading zeros in any octet
+   OR ip REGEXP '(^|\\.)([3-9][0-9]{2,}|2[6-9][0-9]|25[6-9])(\\.|$)'  -- Numbers greater than 255
+   OR ip NOT REGEXP '^([0-9]{1,3}\\.){3}[0-9]{1,3}$'  -- Not exactly 4 octets
+GROUP BY ip
+ORDER BY invalid_count DESC, ip DESC;
