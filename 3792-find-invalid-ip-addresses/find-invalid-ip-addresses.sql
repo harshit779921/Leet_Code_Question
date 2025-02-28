a994620d997bee8ac07a68329f03cd427a4ef15d
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-02-28 22:22:30 +0530

    Time: 1022 ms (5.20%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3792-find-invalid-ip-addresses/find-invalid-ip-addresses.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT ip,
+       count(*) AS invalid_count
+  FROM logs
+ WHERE NOT regexp_like(ip, '^(?:[1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(?:[.](?:[1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$')
+ GROUP BY ip
+ ORDER BY invalid_count DESC,
+          ip DESC;
