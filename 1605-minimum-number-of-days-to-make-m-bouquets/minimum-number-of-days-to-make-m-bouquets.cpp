4428af421306ed62e1f1c3b1f01bda1929e3fe58
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-03-07 15:56:07 +0530

    Time: 23 ms (59.61%) | Memory: 70.5 MB (47.68%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    bool check(vector<int>& bloomDay, int m, int k, int mid) {
-        int cnt = 0, bouquet = 0;
-        for (int i = 0; i < bloomDay.size(); i++) {
-            if (bloomDay[i] <= mid) {
-                cnt++;
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int low = bloomDay[0];
+        int high = bloomDay[0];
+        int n = bloomDay.size();
+        for (int i = 0; i < n; i++) {
+            low = min(low, bloomDay[i]);
+            high = max(high, bloomDay[i]);
+        }
+        int maxInArray = high;
+        while (low <= high) {
+            int mid = (low + high)/2;
+            int num = 0;
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (bloomDay[i] > mid) {
+                    num += count/k;
+                    count = 0;
+                } else {
+                    count++;
+                }
+            }
+            num += count/k;
+            if (num < m) {
+                low = mid + 1;
             } else {
-                bouquet += (cnt / k);
-                cnt = 0;
+                high = mid - 1;
             }
         }
-        bouquet += (cnt / k);
-        if (bouquet >= m)
-            return true;
-        else
-            return false;
-    }
-
-public:
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        int low = bloomDay[0], high = bloomDay[0];
-        for (int i = 1; i < bloomDay.size(); i++) {
-            low = min(bloomDay[i], low);
-            high = max(bloomDay[i], high);
-        }
-        long long val = (long long)m * (long long)k;
-        if (val > bloomDay.size())
-            return -1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (check(bloomDay, m, k, mid)) {
-                high = mid - 1;
-            } else
-                low = mid + 1;
-        }
-        return low;
+        return (low <= maxInArray) ? low : -1;
     }
 };
