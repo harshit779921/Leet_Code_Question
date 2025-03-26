42893c87307e6f3ce10e64cf22fe57a0e649d012
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-03-26 23:39:22 +0530

    Time: 3 ms (37.19%) | Memory: 113.6 MB (72.37%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        double currSum = 0, maxSum = 0;
-        
-        // Initialize currSum and maxSum to the sum of the initial k elements
-        for (int i = 0; i < k; i++)
-            currSum += nums[i];
-        maxSum = currSum;
-        
-        // Start the loop from the kth element 
-        // Iterate until you reach the end
-        for (int i = k; i < nums.size(); i++) {
-            // Subtract the left element of the window
-            // Add the right element of the window
-            currSum += nums[i] - nums[i - k];
-            
-            // Update the max
-            maxSum = max(maxSum, currSum);
+        int n = nums.size();
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
         }
-        
-        // Since the problem requires average, we return the average
-        return maxSum / k;
+        int maxi = sum;
+        for (int i = k; i < n; i++) {
+            sum += nums[i] - nums[i - k];
+            maxi = max(maxi, sum);
+        }
+        return (double)maxi / k;
     }
 };
