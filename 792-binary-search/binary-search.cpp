a76abd712a12be92be68c63844cbc5cdaf5531a3
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-04-27 19:39:17 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (9.60%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -14,5 +14,18 @@ public:
                 high = mid-1;
         }
         return -1;
+
+        // Recursive Code
+        // int low , high ;
+        // if (low>high)
+        //     return -1;
+
+        // int mid = (low+high)/2;
+
+        // if(nums[mid] == target)
+        //     return mid;
+        // else if(target>nums[mid])
+        //     return search(nums,mid+1,high,target);
+        // return search(nums,low,mid-1,target);
     }
 };
