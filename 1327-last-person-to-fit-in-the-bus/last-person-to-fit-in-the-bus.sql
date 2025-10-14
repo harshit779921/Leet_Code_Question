eb1e0c64a7d6ff3e9956dcecea2b3ac46ca13b99
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-10-14 22:34:41 +0530

    Time: 773 ms (66.47%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,9 +1,13 @@
 # Write your MySQL query statement below
+WITH CTE AS (
 SELECT 
-    q1.person_name
-FROM Queue q1 JOIN Queue q2 
-ON q1.turn >= q2.turn
-GROUP BY q1.turn
-HAVING SUM(q2.weight) <= 1000
-ORDER BY SUM(q2.weight) DESC
-LIMIT 1
+person_name,
+SUM(weight) OVER (ORDER BY turn) AS cumulative_weight
+FROM Queue
+)
+SELECT 
+person_name
+FROM CTE
+WHERE cumulative_weight <= 1000
+ORDER BY cumulative_weight DESC
+LIMIT 1;
