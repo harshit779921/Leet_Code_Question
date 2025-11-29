de969df12e6945b59d13c23b525ea47c46b5ba6e
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-11-29 11:51:30 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (34.12%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -8,8 +8,8 @@ class Solution:
 
         # Optimal Solution
         num_map = {}  # Hash table to store number and its index
-        for i, num in enumerate(nums):
-            complement = target - num  # Find the complement
-            if complement in num_map:
-                return [num_map[complement], i]  # Return indices of complement and current number
-            num_map[num] = i  # Store the number with its index
+        for i in range(0, len(nums)):
+            remaining = target - nums[i]  # Find the remaining
+            if remaining in num_map:
+                return [num_map[remaining], i]  # Return indices of remaining and current number
+            num_map[nums[i]] = i  # Store the number with its index
