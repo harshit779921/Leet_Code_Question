4420712f3b5d483dcb5bee2c27865dc2eec7aa93
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-11-29 11:47:40 +0530

    Time: 3 ms (49.78%) | Memory: 18.8 MB (51.46%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,6 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(0, len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j] == target:
-                    return [i,j]
+        # Brute
+        # for i in range(0, len(nums)-1):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i]+nums[j] == target:
+        #             return [i,j]
+
+        # Optimal Solution
+        num_map = {}  # Hash table to store number and its index
+        for i, num in enumerate(nums):
+            complement = target - num  # Find the complement
+            if complement in num_map:
+                return [num_map[complement], i]  # Return indices of complement and current number
+            num_map[num] = i  # Store the number with its index
