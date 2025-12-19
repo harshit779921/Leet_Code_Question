25cb03b542c6c0e7a7b0b6e1a4a83f2a8fc02c23
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2025-12-19 21:15:34 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (53.35%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,21 +1,24 @@
 class Solution(object):
-    # Using STL
-    # def merge(self, nums1, m, nums2, n):
-    #   for j in range(n):
-    #       nums1[m+j] = nums2[j]
-    #   nums1.sort()
-
-# Using Two Pointer Approach
     def merge(self, nums1, m, nums2, n):
-        i = m - 1
-        j = n - 1
-        k = m + n - 1
-        
-        while j >= 0:
-            if i >= 0 and nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                i -= 1
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        if n == 0 :return
+        len1 = len(nums1)
+        end_idx = len1-1
+        while n > 0 and m > 0 :
+            if nums2[n-1] >= nums1[m-1]:
+                nums1[end_idx] = nums2[n-1]
+                n-=1
             else:
-                nums1[k] = nums2[j]
-                j -= 1
-            k -= 1
+                nums1[end_idx] = nums1[m-1]
+                m-=1
+            end_idx-=1
+        while n > 0:
+            nums1[end_idx] = nums2[n-1]
+            n-=1
+            end_idx-=1
